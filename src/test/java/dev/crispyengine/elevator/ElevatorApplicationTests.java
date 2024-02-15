71e9f92abadbd17f7b24a2c257e8e3c376e9950e
Rishabh Joshi <rishabh9@gmail.com>
2024-02-15 21:07:24 +0000

    Add project base

---
+++ src/test/java/dev/crispyengine/elevator/ElevatorApplicationTests.java
@@ -0,0 +1,14 @@
+package dev.crispyengine.elevator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Remove me.
+	}
+
+}
